835744a94835900ccaff056ed70fbc0ba533f582
Nigam kumar <nigam.kumar@soprasteria.com>
2025-04-06 17:45:46 +0530

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Template/', // 👈 Use your GitHub repo name here
   plugins: [react()],
 })
